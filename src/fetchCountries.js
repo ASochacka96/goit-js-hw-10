b6ecd34a6212d6f2e1e51224e1cb804e5c173c7a
Anna <a.sochacka96@gmail.com>
2024-05-07 10:15:20 +0200

    Solution of homework

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,17 +1,3 @@
-// export const fetchCountries = name => {
-//     const BASE_URL = 'https://restcountries.com/v3.1/name/';
-//     const properties = 'fields=name,capital,population,flags,languages';
-
-//     return fetch(`${BASE_URL}${name}?${properties}`).then(response => {
-//         console.log(!response.ok);
-//         if (!response.ok) {
-//             throw new Error(response.status);
-//         }
-//         return response.json();
-//     });
-// };
-
-
 const fields = '?fields=name,capital,population,flags,languages';
 
 export const fetchCountries = name => {
